f7372f6eb828d188655455f2cce87a164069528f
Utishnik <ykokoyto@bk.ru>
2024-07-17 15:45:22 +0300

    Я конечно все понимаю но то что когда я переместил проект с visuaul studia на code block(через mingw) и у меня все стало нормально работать

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Tools.h"
-//todo : https://www.cyberforum.ru/cpp-beginners/thread2709026.html решить эту проблему добавить возможность разносить шаблоны на основе tpp(но это не точно) 
+//todo : https://www.cyberforum.ru/cpp-beginners/thread2709026.html решить эту проблему добавить возможность разносить шаблоны на основе tpp(но это не точно)
 
 int main()
 {
@@ -10,27 +10,25 @@ int main()
 	test1.height = 3;
 	test1.widht = 4;
 	test1.depth = 5;
-		
+
 	test.height = 3;
 	test.widht = 4;
 
 	test.map = _Rect_Malloc_2d<float>(test.height, test.widht);
 	test1.map = _Rect_Malloc_3d<float>(test1.height, test1.widht, test1.depth);//ошибка походу тут
 
-	//ошибка возникает выше
-	printf("d  %p\n",test.map);
-	for (int i = 0; i < 2; i++) free(test.map[i]);
-	//free(arr1);
-	//for (int i = 0; i < 3; i++)
-	//{
-	//	for (int j = 0; j < 4; j++)
-	//	{
-	//		arr1[i][j] = (float)(i);
-	//		printf("%g ", arr1[i][j]);
-	//	}
-	//	printf("\n");
-	//}
-	//_Rect_Free_2d(test);
+    //_Rect_Free_2d(test);
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			test.map[i][j] = (float)(i);
+			printf("%g ", test.map[i][j]);
+		}
+		printf("\n");
+	}
+	_Rect_Free_2d(test);
 
 	/*
 	int* gg = (int*)malloc(sizeof(int) * 10);
